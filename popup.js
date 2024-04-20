9e6fa816a00a3d6652a9ab269dcc5ef6c0237dd0
Yiran Wan <ewan951218@gmail.com>
2024-04-20 08:51:03 -0700

    add some ui for questions

--- popup.js
+++ popup.js
@@ -9,39 +9,62 @@ const onDelete = e => {};
 
 const setBookmarkAttributes =  () => {};
 
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
+  
+
   // Fetch current tab URL and extract video ID
-  var questionContainer = document.getElementById('questions');
+  // document.documentElement.requestFullscreen();
+  var questionList = document.getElementById('questions');
   var questionTemplate = document.getElementById('question-template');
 
   // hardcoded data
   var data = [
     {
       "question": "What is the name of the Sims 4 kit reviewed in the transcript?",
-      "answers": [
+      "choices": [
         "A. Party Time Kit",
         "B. Let's Get Festive Kit",
         "C. Birthday Bash Kit",
         "D. It is not mentioned in the transcript"
       ],
-      "correct_answer": 0
+      "answer": 0
     },
     {
       "question": "Does the reviewer recommend this kit?",
-      "answers": [
+      "choices": [
         "A. Yes, especially for console players.",
         "B. Yes, it is a great value.",
         "C. No, she thinks it is overpriced and lacking in features.",
         "D. No, but she recommends similar free custom content for PC players."
       ],
-      "correct_answer": 2
+      "answer": 2
     }]
 
   function createQuestionElement(datum){
     var clone = questionTemplate.content.cloneNode(true);
     var questionElement = clone.querySelector('.question');
     var questionTextElement = clone.querySelector('.question-text');
+    var choicesElement = clone.querySelector('.choices');
     questionTextElement.textContent = datum.question;
+    // append choices
+    datum.choices.forEach((choice)=>{
+      let label = document.createElement("label");
+      let input = document.createElement("input");
+      input.type = 'radio';
+      input.name = datum.question;
+      input.value = choice;
+      // input.id = choice;
+      // label.for = choice; 
+      label.appendChild(input);
+      label.appendChild(document.createTextNode(choice));
+      label.appendChild(document.createElement("br"));
+      choicesElement.appendChild(label);
+    })
     return questionElement;
   }
 
@@ -54,7 +77,7 @@ document.addEventListener("DOMContentLoaded", () => {
       // chrome.tabs.sendMessage(tabs[0].id, { url: videoId, action: 'quiz' });
       // TODO: use videoId to get transcript then generate formated quiz
       data.forEach(datum => {
-        questionContainer.appendChild(createQuestionElement(datum));
+        questionList.appendChild(createQuestionElement(datum));
       });
     }
     
